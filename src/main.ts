8613cd8718271629254993f2064e6ffab556b59d
Tuxtasinboyev Omadbek <tuxtasinboyev@kali.kali>
2025-09-08 17:47:43 +0500

    buldi

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
