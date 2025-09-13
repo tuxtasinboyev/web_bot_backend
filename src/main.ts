3550606de0f67c7027208f5a9004c2aff8f934b1
Tuxtasinboyev Omadbek <tuxtasinboyev@kali.kali>
2025-09-13 22:30:25 +0500

    buldi

--- src/main.ts
+++ src/main.ts
@@ -9,9 +9,10 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
   app.enableCors({
-    origin: "*"
+    origin: ['https://salomnn.netlify.app/login'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
   });
 
 
